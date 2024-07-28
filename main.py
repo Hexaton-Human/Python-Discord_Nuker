3ba0c2f2c9ee7ce434613506ff07c77671af66c8
Hexaton-Human <167614260+Hexaton-Human@users.noreply.github.com>
2024-07-28 10:52:27 +0900

    Add download_requirement function

---
+++ main.py
@@ -0,0 +1,21 @@
+import pip
+import os
+
+downloaded = false
+
+def download_requirement():
+  if downloaded == false
+    pip.main(['install discord'])
+    pip.main(['install pystyle'])
+    pip.main(['install requests'])
+    pip.main(['install json'])
+    pip.main(['install random'])
+    pip.main(['install traceback'])
+    os.system("cls")
+    print("module download is done.")
+    print("ready to run nuker.")
+  downloaded = true
+
+def main():
+  if downloaded == true:
+    print("main")
